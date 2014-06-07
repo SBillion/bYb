8e7d3e26872f27362f61422d3d5bf60fe3f70ae2
Sébastien BILLION <sebastien.billion@gmail.com>
2014-06-07 17:30:07 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -19,8 +19,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME':'43844_byb',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'USER': 'slartibartfast',                      # Not used with sqlite3.
+        'PASSWORD': 'trythisone',                  # Not used with sqlite3.
         'HOST': 'mysql1.alwaysdata.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
